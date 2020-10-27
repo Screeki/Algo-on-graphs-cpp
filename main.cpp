e5250a3a04c378248ce5132cebb498ef66a72549
Screeki <49313741+Screeki@users.noreply.github.com>
2020-10-27 14:18:03 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include "Graph.h"
 
+const int nH = 5;
+const int highNumber = +INFINITY;
+
 int main()
 {
-	Graph G(9);
-	G.enter_vector_vertice();
-	G.bfs(0);
-	G.findDeWay(0, 4);
+	//Init
+	int graph[nH][nH];
+	for (size_t j = 0; j < nH; j++)
+	{
+		for (size_t i = 0; i < nH; i++)
+		{
+			cin >> graph[j][i];
+		}
+	}
+
+	//Algorithm Deixtra
+	int start = 0;
+	int finish = 4;
+	int lenght;
+	vector<vector<int>> visited(nH);
+	visited[start][start] = 1;
+	for (size_t i = 0; i < nH; i++)
+	{
+		for (size_t curent = 0; curent < nH; curent++)
+		{
+			if (!visited[i][curent])
+			{
+				visited[i][curent] = 1;
+				if (graph[i][curent])
+				{
+
+				}
+			}
+		}
+	}
 }
